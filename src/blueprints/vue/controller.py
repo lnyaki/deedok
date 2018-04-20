7d6d902e7d594ed66c2bb1c08cd25bfbff0e3ea0
loic <loic.nyaki@gmail.com>
2018-04-20 04:08:17 +0200

    Linked the vue blueprint. It works correctly.

--- src/blueprints/vue/controller.py
+++ src/blueprints/vue/controller.py
@@ -6,9 +6,10 @@ import 	blueprints.vue.page_blocks 		as blocks
 def index():
 	page 		= Page('Vue.js tests!')
 	graph 		= Graph_Dao()
+	data 		= {}
 
 	page.addScript("dbQueries.js","gsearch")
 
-	page.addBlockCenter(blocks.vue_test())
+	page.addBlockCenter(blocks.vue_test(data))
 	
 	return page.render()
--- src/blueprints/vue/routes.py
+++ src/blueprints/vue/routes.py
@@ -3,12 +3,13 @@ from . import controller
 
 from blueprints.gsearch import ajax_controller as ajx
 from blueprints.vue import controller as vue_controller
-import jboost_controller as jbc
 
 def routes(app):
+
 	@app.route('/',methods=['GET'])
-	def shokw():
-        	return "hello"
+	def index():
+		data = {}
+		return vue_controller.index()
 
 	@app.route('/test', methods=['GET'])
 	def test():
