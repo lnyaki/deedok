85ce918ab2a9b8c92f10f7c576ab1ebc98dddf2f
loic <loic.nyaki@gmail.com>
2018-09-18 17:35:43 +0200

    Adding a script that will always fail, in order to better test my pipeline

--- testScriptFail.sh
+++ testScriptFail.sh
@@ -1,6 +1,8 @@
-echo "*******    Clean Script .*******************************"
+echo "*******    Failing script.*******************************"
 echo "This is a test script"
 echo "We are at "
 pwd
+echo "But this script will fail on purpose"
 echo "********************************************************"
 
+exit 1
